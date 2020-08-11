2088643ca56ae34757593994c0a82a5107e5833a
spjy <spjy@hawaii.edu>
2020-08-11 13:50:29 -1000

    fix: event queue loop

--- source/include/mongo/maintain_event_queue.h
+++ source/include/mongo/maintain_event_queue.h
@@ -6,14 +6,19 @@
 void maintain_event_queue(std::string &agent_list, std::string &shell);
 
 void maintain_event_queue(std::string &agent_list, std::string &shell) {
-    std::string list;
+    while (agent->running()) {
 
-    list = execute("\"" + agent_list + " neutron1 exec get_event\"", shell);
+        std::string list;
 
-    std::string node_type = "event_queue";
+        list = execute("\"" + agent_list + " neutron1 exec get_event\"", shell);
 
-    if (!list.empty()) {
-        send_live("WS Event Queue", node_type, list);
+        std::string node_type = "event_queue";
+
+        if (!list.empty()) {
+            send_live("WS Event Queue", node_type, list);
+        }
+
+        COSMOS_SLEEP(5.);
     }
 }
 
