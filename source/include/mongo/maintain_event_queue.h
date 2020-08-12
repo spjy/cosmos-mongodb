8820d892289a290d21165a6e285f578b5a42e0a4
spjy <spjy@hawaii.edu>
2020-08-11 16:07:38 -1000

    feat: check for proper json

--- source/include/mongo/maintain_event_queue.h
+++ source/include/mongo/maintain_event_queue.h
@@ -12,14 +12,19 @@ void maintain_event_queue(std::string &agent_list, std::string &shell) {
 
         list = execute("\"" + agent_list + " neutron1 exec get_event\"", shell);
 
-        std::string node_type = "event_queue";
+        try {
+            auto json = bsoncxx::from_json(list);
+            std::string node_type = "event_queue";
 
-        if (!list.empty()) {
-            list.pop_back();
+            if (!list.empty()) {
+                list.pop_back();
 
-            std::string response = "{\"queue\":\"" + escape_json(list) + "\", \"node_type\": \"event_queue\"}";
+                std::string response = "{\"queue\":\"" + escape_json(list) + "\", \"node_type\": \"event_queue\"}";
 
-            send_live("WS Event Queue", node_type, response);
+                send_live("WS Event Queue", node_type, response);
+            }
+        } catch (const bsoncxx::exception &err) {
+            cout << "WS Event Queue: Error converting to BSON from JSON" << err.what() << endl;
         }
 
         COSMOS_SLEEP(5.);
