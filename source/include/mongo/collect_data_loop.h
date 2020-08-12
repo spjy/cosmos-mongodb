f03021bddba92a244dd5894e4b27a6265c818317
spjy <spjy@hawaii.edu>
2020-08-11 15:34:50 -1000

    fix: soh message

--- source/include/mongo/collect_data_loop.h
+++ source/include/mongo/collect_data_loop.h
@@ -132,7 +132,7 @@ void collect_data_loop(mongocxx::client &connection_live, std::string &realm, st
 
                     if (whitelisted_node(included_nodes, excluded_nodes, node)) {
                         if (token.length() > 10 && difftime(currentTime, startTime) >= 1800) {
-                            std::string json_string = "{\"block\": [{ \"type\": { \"text\": \"mrkdwn\", \"text\": \"Activity detected after 30 minutes! ```" + message.jdata + "```\" } }, { \"type\": \"divider\" }]}";
+                            std::string json_string = "{\"block\": [{ \"type\": { \"text\": \"mrkdwn\", \"text\": \"Activity detected after 30 minutes! ```" + list + "```\" } }, { \"type\": \"divider\" }]}";
                             try {
                                 cout << "POST request to http://hooks.slack.com" << endl;
                                 auto response = client.request("POST", token, json_string);
